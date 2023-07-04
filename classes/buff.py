404c0c2df42bea951e729f402c4092705dfd69b6
perrebser <workperrebser@gmail.com>
2023-07-04 20:52:26 +0200

    remove unused method

--- classes/buff.py
+++ classes/buff.py
@@ -13,26 +13,6 @@ class Buff:
         r = requests.get(URL).json()
         rate = 1 / float(r["rates"][pair]["rate"])
         return rate
-
-    def get_price_item(self, itemID, rate, numOffersToCheck):
-        URL = "https://buff.163.com/api/market/goods/sell_order"
-        params = {
-            "game": "csgo",
-            "page_num": "1",
-            "goods_id": itemID
-        }
-        r = requests.get(URL, params=params).json()
-        items = r["data"]["items"][:numOffersToCheck]
-        data = []
-        for item in items:
-            price = float(item["price"])
-            price_usd = round(float(price * rate), 2)
-            # if the item has attributes(phase on doppler or % on fades get them)
-            metaphysic = item["asset_info"]["info"].get("metaphysic", {}).get("data", {}).get("name", {})
-            item_data = {"price": price, "priceUSD": price_usd, "Phase/Fade": metaphysic}
-            data.append(item_data)
-        return data
-
     async def fetch_item_price(self, session, itemID, rate, numOffersToCheck):
         URL = "https://buff.163.com/api/market/goods/sell_order"
         params = {
